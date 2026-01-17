4fa65265f30a74029da21e8faadaa438701e31ff
mspcris <msp.cris@gmail.com>
2026-01-17 17:54:31 -0300

    Tracktime no modal.

--- tracktime/urls.py
+++ tracktime/urls.py
@@ -5,4 +5,16 @@ app_name = "tracktime"
 
 urlpatterns = [
     path("", views.portal, name="portal"),
+
+    # card modal
+    path(
+        "cards/<int:card_id>/track-time/",
+        views.card_tracktime_panel,
+        name="card_panel",
+    ),
+    path(
+        "cards/<int:card_id>/track-time/manual/",
+        views.card_tracktime_manual,
+        name="card_manual",
+    ),
 ]
--- tracktime/views.py
+++ tracktime/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
-@login_required
 def portal(request):
     return render(request, "tracktime/portal.html")
+
+def card_tracktime_panel(request, card_id):
+    return render(
+        request,
+        "tracktime/card_tracktime_panel.html",
+        {"card_id": card_id},
+    )
+
+def card_tracktime_manual(request, card_id):
+    return render(
+        request,
+        "tracktime/card_tracktime_manual.html",
+        {"card_id": card_id},
+    )
